24037d5773f49008365d7343097abbb06c96b6c5
Fernando Araujo <fernando@Fernando-Note.local>
2018-02-04 20:41:22 -0200

    Test Projects

---
+++ tests/ANC.Sales.Commom.Test/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ANC.Sales.Commom.Extensions;
+using Xunit;
+
+namespace ANC.Sales.Commom.Test.Extensions
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void IsValidEmail_WithInvalidEmail_ShouldReturnFalse()
+        {
+            var itsNotAnEmail = "a";
+
+            var t = itsNotAnEmail.IsValidEmail();
+            Assert.NotNull(t);
+        }
+    }
+}
---
+++ tests/ANC.Sales.Data.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ANC.Sales.Data.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
